f4de1ed4c40e0049eb51e2ca018d58aaf2fa08c9
KAMILA <dudekkamila@icloud.com>
2023-11-02 13:08:36 +0100

    added hooks to aplication todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,83 @@
 import './App.scss';
-import {useState} from "react";
-
+import {Headline} from "./components/Headline.jsx";
+import TodoAdd from "./components/TodoAdd.jsx";
+import TodoList from "./components/TodoList.jsx";
+import Counter from "./components/Counter.jsx";
+import {DeleteAllBtn} from "./components/DeleteAllBtn.jsx";
+import useLocalStorage from "./hooks/useLocalStorage.js";
 const getId = (todos) => todos.length === 0 ? 1 : Math.max(...todos.map((task) => task.id)) + 1;
 
+
 function App() {
-    const [todo, setTodo] = useState('');
-    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useLocalStorage('task');
+    const [todos, setTodos] = useLocalStorage('tasks');
 
     // const getId = () => todos.length === 0 ? 1 : Math.max(...todos.map((task) => task.id)) + 1;
-
+    //
+    // useEffect(() => {
+    //     setTodos((getFromLocalStorage ()))
+    // }, []);
 
     const handleAddTodo = (evt) => {
         if (evt.key === "Enter" && todo.trim().length >= 3) {
-            setTodos([{
-                id: getId(todos),
+           setTodos([{
+                id: getId(todos ),
                 status: 'in progress',
                 title: todo,
-
             }, ...todos]);
+
             setTodo('');
+
         }
     };
     const handleChangeStatus = (task) => {
-        task.status = task.status === 'in progress'? 'done' : 'in progress';
+        task.status = task.status === 'in progress' ? 'done' : 'in progress';
         setTodos([...todos]);
 
     };
 
     const handleDeleteTodo = (todo) => {
-        setTodos(todos.filter((task)=> task !== todo));
+        setTodos(todos.filter((task) => task !== todo));
 
     };
 
-    const handleDeleteDoneTasks = ()  =>{
-        setTodos(todos.filter((task)=> task.status !== 'done'));
+    const handleDeleteDoneTasks = () => {
+        setTodos(todos.filter((task) => task.status !== 'done'));
     }
+
+
 
     return (
         <div className="todoapp">
-            <h1>todos</h1>
+            <Headline/>
             <section className="todos">
-                <input
-                    type="text"
-                    className="todo-input"
-                    value={todo}
-                    placeholder="What needs to be done?"
-                    onChange={(event) => setTodo(event.target.value)}
-                    onKeyUp={handleAddTodo}
+                <TodoAdd
+                    todo={todo}
+                    setTodo={setTodo}
+                    addTodo={handleAddTodo}
                 />
-                <ul className="todos-list">
-                    {todos.map((task) => (
-                        <li
-                            className="todos-item"
-                            key={task.id}
-                        >
-                            <span
-                                className={task.status === 'in progress'? 'status' : 'status done'}
-                                onClick={() => handleChangeStatus(task)}
-
-                            ></span>
-                            <span>{task.title}</span>
-                            <button
-                                className='btn-delete'
-                                onClick={() =>handleDeleteTodo(task)}
 
 
-                            >delete</button>
-                        </li>
-                    ))}
-                </ul>
+                <TodoList
+
+                todos={todos}
+                handleChangeStatus ={handleChangeStatus}
+                handleDeleteTodo={handleDeleteTodo}
+                />
+
+
                 <div className='box'>
+                    <Counter
+                        todos={todos}
+                    />
 
-                <p className='counter'>{todos.filter((task)=> task.status === 'in progress').length } items left</p>
-                    {todos.some((task)=> task.status === 'done' ) && (
-                <button
-                    className="btn"
-                    onClick={()=> handleDeleteDoneTasks}
 
-                >Clear </button>
-                        )}
+                    {todos.some((task) => task.status === 'done') && (
+                        <DeleteAllBtn
+                            handleDeleteDoneTasks={ handleDeleteDoneTasks}
+                        />
+                    )}
                 </div>
-
 
 
             </section>
